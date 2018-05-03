6d492909cc60a0849ff366eb61fe8192ca4ddc64
CaryLorrk <carylorrk@gmail.com>
2018-05-03 14:01:21 +0800

    tensorflow build with SIMD

--- apps/tensorflow/build.sh
+++ apps/tensorflow/build.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`/tensorflow
 ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1
-bazel build -c opt --config=cuda --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0" \
+bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2 --config=cuda --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0" \
         tensorflow/tools/pip_package:build_pip_package && \
     bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/pip && \
     pip --no-cache-dir install --upgrade /tmp/pip/tensorflow-*.whl && \
